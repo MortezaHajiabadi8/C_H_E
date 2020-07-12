54f1e896af259c39a82a04ebadb239a0b895f428
mortezahajiabadi <hajiabadi1377@gmail.com>
2020-07-12 14:51:49 +0430

    edit find_contours

--- Gathering-Data-From-the-Form.py
+++ Gathering-Data-From-the-Form.py
@@ -60,46 +60,10 @@ def thresholdedForm(src):
     # cv2.imshow('Thresholded', T)
     # cv2.waitKey()
     return T
-
-
-def find_lines(inverted, thresholded):
-    #following lines are for find lines in image and then boxes
-
-    # Length(width) of kernel as 100th of total width
-    kernel_len = np.array(inverted).shape[1]//100
-    # Defining a vertical kernel to detect all vertical lines of image 
-    ver_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, kernel_len))
-    # Defining a horizontal kernel to detect all horizontal lines of image
-    hor_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (kernel_len, 1))
-    # A kernel of 2x2
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
-
-    #Use vertical kernel to detect and save the vertical lines in a jpg
-    image_1 = cv2.erode(thresholded, ver_kernel, iterations=3)
-    vertical_lines = cv2.dilate(image_1, ver_kernel, iterations=3)
-    # cv2.imshow("vertical", vertical_lines)
-    # cv2.waitKey()
-
-    #Use horizontal kernel to detect and save the horizontal lines in a jpg
-    image_2 = cv2.erode(thresholded, hor_kernel, iterations=3)
-    horizontal_lines = cv2.dilate(image_2, hor_kernel, iterations=3)
-    # cv2.imshow("horizontal", horizontal_lines)
-    # cv2.waitKey()
-
-    # Combine horizontal and vertical lines in a new third image, with both having same weight.
-    img_vh = cv2.addWeighted(vertical_lines, 0.5, horizontal_lines, 0.5, 0.0)#Eroding and thesholding the image
-    img_vh = cv2.erode(~img_vh, kernel, iterations=2)
-    thresh, img_vh = cv2.threshold(img_vh,128,255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-    # cv2.imshow("img_vh", img_vh)
-    # cv2.waitKey()
-    return img_vh
-
+ 
 def find_contours(src):
-    # Detect contours for following box detection
     _, contours, _ = cv2.findContours(src, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    # Sort all the contours by top to bottom.
-    contours, boundingBoxes = sort_contours(contours, method="top-to-bottom")
     return contours
 
 def find_fields_and_boxes(contours):
